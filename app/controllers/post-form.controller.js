d1550d096aac8fe27e572842ecb1303da1ab07eb
144mdgross <144.mdgross@gmail.com>
2017-05-06 20:51:41 -0600

    comments pluralize correctly. next up hide/show comments for individual posts.

--- app/controllers/post-form.controller.js
+++ app/controllers/post-form.controller.js
@@ -14,22 +14,15 @@
 
     vm.$onInit = populate
     vm.createComment = createComment
-    vm.comments = []
     // see vm.votes below scope variables
 
 
     vm.createPost = function createPost(event, author) {
       event.preventDefault()
-      let buildPost = {
-        title: vm.post.title,
-        author: vm.post.author,
-        body: vm.post.body,
-        time: '1 hour ago',
-        image: vm.post.image,
-        votes: 0,
-        comments: []
-      }
-      vm.posts.push(buildPost)
+      vm.post.votes = 0,
+      vm.post.time = new Date,
+      vm.post.comments = []
+      vm.posts.push(vm.post)
       delete vm.author
     }
 
@@ -63,20 +56,25 @@
         title: 'AORTA is the best',
         author: 'Esteban Kelly',
         body: 'the Anit-Opression Resource and Training Alliance is a great resource for everyone.',
-        time: '1 hour ago',
+        time: new Date(),
         image: 'http://aorta.coop/sites/default/files/imagecache/trainer_about_image/dsc_03111_0.jpg',
-        votes: 0
+        votes: 10,
+        comments: []
+      }, {
+        title: 'AORTA is the best',
+        author: 'Esteban Kelly',
+        body: 'the Anit-Opression Resource and Training Alliance is a great resource for everyone.',
+        time: new Date(),
+        image: 'http://aorta.coop/sites/default/files/imagecache/trainer_about_image/dsc_03111_0.jpg',
+        votes: 10,
+        comments: []
       }]
     }
 
-    function createComment(event, comment) {
+    function createComment(event, post) {
       event.preventDefault()
-      let opinion = {
-        text: vm.comment
-      }
-
-      vm.comments.push(opinion)
-      delete vm.comment
+      post.comments.push(post.comment)
+      delete post.comment
     }
 
   } // end of BindForm
