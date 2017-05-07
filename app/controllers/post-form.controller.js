fc6c66bf1ff4a61a231a9dd87c567b3c1eb569ea
144mdgross <144.mdgross@gmail.com>
2017-05-07 16:12:16 -0600

    users can sort based off of votes, title or date

--- app/controllers/post-form.controller.js
+++ app/controllers/post-form.controller.js
@@ -14,40 +14,38 @@
 
     vm.$onInit = populate
     vm.createComment = createComment
+    vm.sort = sort
+
+
     // see vm.votes below scope variables
 
 
     vm.createPost = function createPost(event, author) {
       event.preventDefault()
-      vm.post.votes = 0,
-      vm.post.time = new Date,
+      vm.post.votes = 0
+      vm.post.date = new Date
       vm.post.comments = []
       vm.posts.push(vm.post)
       delete vm.author
     }
 
     $scope.val = 2
-    $scope.commentVal = 2
-    $scope.commentEven = true
     $scope.even = true
-    // this is for ngShow
+    $scope.options = ['votes', 'title', 'date']
+    $scope.selected = $scope.options[0]
+    vm.select = $scope.selected
+        console.log("select", vm.select);
+    // controlls ngShow on create post form
     $scope.counter = function() {
       $scope.val += 1
       $scope.even = $scope.val % 2 === 0
     }
 
-    $scope.commentCounter = function() {
-      $scope.commentVal += 1
-      $scope.commentEven = $scope.commentVal % 2 === 0
-    }
-
     $scope.voteUp = function(post) {
-      console.log("voting up");
       post.votes += 1
     }
 
     $scope.voteDown = function (post) {
-      console.log("voting down");
       post.votes === 0 ? post.votes = 0 : post.votes -= 1
     }
 
@@ -56,25 +54,34 @@
         title: 'AORTA is the best',
         author: 'Esteban Kelly',
         body: 'the Anit-Opression Resource and Training Alliance is a great resource for everyone.',
-        time: new Date(),
+        date: new Date(),
         image: 'http://aorta.coop/sites/default/files/imagecache/trainer_about_image/dsc_03111_0.jpg',
         votes: 10,
         comments: []
+
       }, {
-        title: 'AORTA is the best',
-        author: 'Esteban Kelly',
-        body: 'the Anit-Opression Resource and Training Alliance is a great resource for everyone.',
-        time: new Date(),
-        image: 'http://aorta.coop/sites/default/files/imagecache/trainer_about_image/dsc_03111_0.jpg',
-        votes: 10,
-        comments: []
+        title: 'Ed Whitfeild on Education and Reparations',
+        author: 'Amber Brown',
+        body: 'Know the difference between equity and equality.',
+        date: new Date(),
+        image: 'http://community-wealth.org/sites/clone.community-wealth.org/files/ed-whitfield-usfwc-2012.jpg',
+        votes: 9,
+        comments: [{text:'Make America Pay Reparations'}]
+
       }]
     }
 
     function createComment(event, post) {
       event.preventDefault()
+      if(post.comment !== undefined) {
+        console.log(post.comment);
       post.comments.push(post.comment)
+      }
       delete post.comment
+    }
+
+    function sort(choice) {
+      $scope.selected = choice
     }
 
   } // end of BindForm
